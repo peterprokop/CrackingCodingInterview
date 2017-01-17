60f3a3af047ba09380897b9c9b8bf947a7547ca8
Peter Prokop <pitrowech@gmail.com>
2017-01-17 14:34:34 +0100

    Initial Commit

---
+++ CrackingCodingInterview/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CrackingCodingInterview
+//
+//  Created by Peter Prokop on 17/01/2017.
+//  Copyright © 2017 Peter Prokop. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
